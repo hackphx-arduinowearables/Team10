5fcaf33e35b1f10c416974a2d358204b379b23ce
luthermonson <luther.monson@gmail.com>
2014-01-18 13:16:14 -0700

    LED volt meter working

--- xadowglove.ino
+++ xadowglove.ino
@@ -1,16 +1,24 @@
-#include <Xadow.h>
+
 #include <Wire.h>
 #include <LEDMatrix.h>
 
-#define ADDRESS 0x04;
+#define ADDRESS 0x04
+#define VOLTS_CONVERTER 310.0
 
-#define VOLTS_CONVERTER 310.0;
+LEDMatrix fiveBySeven = LEDMatrix(ADDRESS);
+char buffer[5];
+
+
 void setup () {
-     
+   fiveBySeven.setDir(DIR_NORMAL);
 }
 
 void loop () {
+  
   double volts = analogRead(A5) / VOLTS_CONVERTER;
-  Serial.println(volts);
-  delay (50);
+  char* v = dtostrf(volts, 5, 2, buffer);
+  Serial.println(v);
+  
+  fiveBySeven.sendString(STR_ONCE, 100, v);
+  delay (1500);
 }
